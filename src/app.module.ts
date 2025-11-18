e9f2e76443f4119bb98b7b4d13dea8a8ea86da37
DjYenerys <dajarythhernandez@gmail.com>
2025-11-18 18:39:28 -0500

    Configuración despliegue #6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,65 +1,66 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+  import { Module } from '@nestjs/common';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+  import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
 
-import { CategoryModule } from './category/category.module';
-import { InvoiceModule } from './invoice/invoice.module';
-import { OrderModule } from './order/order.module';
-import { OrderDetailModule } from './order-detail/order-detail.module';
-import { PaymentModule } from './payment/payment.module';
-import { PaymentMethodModule } from './pay-methods/pay-method.module';
-import { ProductModule } from './product/product.module';
-import { RoleModule } from './role/role.module';
-import { UserModule } from './user/user.module';
-import { CartModule } from './cart/cart.module';
-import { AuthModule } from './auth/auth.module';
-import { ChatModule } from './chat/chat.module';
+  import { CategoryModule } from './category/category.module';
+  import { InvoiceModule } from './invoice/invoice.module';
+  import { OrderModule } from './order/order.module';
+  import { OrderDetailModule } from './order-detail/order-detail.module';
+  import { PaymentModule } from './payment/payment.module';
+  import { PaymentMethodModule } from './pay-methods/pay-method.module';
+  import { ProductModule } from './product/product.module';
+  import { RoleModule } from './role/role.module';
+  import { UserModule } from './user/user.module';
+  import { CartModule } from './cart/cart.module';
+  import { AuthModule } from './auth/auth.module';
+  import { ChatModule } from './chat/chat.module';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST') || 'localhost',
-        port: configService.get('DATABASE_PORT') || 5432,
-        username: configService.get('DATABASE_USERNAME') || 'postgres',
-        password: configService.get('DATABASE_PASSWORD') || '1947',
-        database: configService.get('DATABASE_NAME') || 'taller5',
-        autoLoadEntities: true,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false,
-        logging: configService.get('NODE_ENV') !== 'production',
-        ssl: configService.get('NODE_ENV') === 'production',
-        extra: configService.get('NODE_ENV') === 'production' ? {
-          ssl: {
-            rejectUnauthorized: false
-          }
-        } : undefined,
+  @Module({
+    imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
       }),
-      inject: [ConfigService],
-    }),
-    ChatModule,
-    AuthModule,
-    CategoryModule,
-    InvoiceModule,
-    OrderModule,
-    OrderDetailModule,
-    PaymentModule,
-    PaymentMethodModule,
-    ProductModule,
-    RoleModule,
-    UserModule,
-    CartModule,
-  ],
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          url: configService.get('DATABASE_URL'),
+          host: configService.get('DATABASE_HOST') || 'localhost',
+          port: configService.get('DATABASE_PORT') || 5432,
+          username: configService.get('DATABASE_USERNAME') || 'postgres',
+          password: configService.get('DATABASE_PASSWORD') || '1947',
+          database: configService.get('DATABASE_NAME') || 'taller5',
+          autoLoadEntities: true,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: false,
+          logging: configService.get('NODE_ENV') !== 'production',
+          ssl: configService.get('NODE_ENV') === 'production',
+          extra: configService.get('NODE_ENV') === 'production' ? {
+            ssl: {
+              rejectUnauthorized: false
+            }
+          } : undefined,
+        }),
+        inject: [ConfigService],
+      }),
+      ChatModule,
+      AuthModule,
+      CategoryModule,
+      InvoiceModule,
+      OrderModule,
+      OrderDetailModule,
+      PaymentModule,
+      PaymentMethodModule,
+      ProductModule,
+      RoleModule,
+      UserModule,
+      CartModule,
+    ],
 
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {}
